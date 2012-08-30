34d5b5c5784c2142cc2c8e686000fbb5120b15fb
rick <rick@a86ed88a-4096-de11-8531-0030489379b8>
2012-08-30 15:04:40 +0000

    AssemblyInfo for GenerateSerializer.

--- trunk/Gibbed.Cryptic.GenerateSerializer/Properties/AssemblyInfo.cs
+++ trunk/Gibbed.Cryptic.GenerateSerializer/Properties/AssemblyInfo.cs
@@ -26,11 +26,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Gibbed.StarTrekOnline.GenerateSerializer")]
-[assembly: AssemblyDescription("")]
+
+[assembly: AssemblyTitle("Gibbed.Cryptic.GenerateSerializer")]
+[assembly: AssemblyDescription("http://gib.me/")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Gibbed.StarTrekOnline.GenerateSerializer")]
+[assembly: AssemblyProduct("Gibbed.Cryptic.GenerateSerializer")]
 [assembly: AssemblyCopyright("Copyright © Gibbed 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -38,10 +39,12 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
+
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5eb66bc8-65cd-4f79-98a6-696c3a428eff")]
+
+[assembly: Guid("e992f187-f9d3-4dc1-b1b1-b3c5d8deb624")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -53,5 +56,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
+
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
